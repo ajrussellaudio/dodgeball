bbda02459b776fd7c0d5a2ee7494f651282cc8fd
Alan Russell <ajrussellaudio@gmail.com>
2016-08-05 15:21:57 +0100

    can create and save Matches

--- models/match.rb
+++ models/match.rb
@@ -6,7 +6,7 @@
 # - away_team_score
 
 # Should be able to:
-# - Create new match
+# - Create new match √
 # - Read match result
 # - Update match (with score?)
 # - Delete match (not really necessary, match declared void?)
@@ -18,7 +18,17 @@ require_relative "../db/sql_runner"
 
 class Match
 
+  attr_accessor :home_team_id, :away_team_id, :home_team_score, :away_team_score
+  attr_reader :id
+
+  def self.all
+    sql = "SELECT * FROM matches;"
+    matches = SqlRunner.run( sql )
+    return matches.map { |match_info| Match.new(match_info) }
+  end
+
   def initialize( options )
+    @id              = options["id"].to_i
     @home_team_id    = options["home_team_id"].to_i
     @away_team_id    = options["away_team_id"].to_i
     @home_team_score = options["home_team_score"].to_i
@@ -28,10 +38,14 @@ class Match
   def save()
     sql = "INSERT INTO matches (
       home_team_id,
-      away_team_id
+      away_team_id,
+      home_team_score,
+      away_team_score
       ) VALUES (
       #{@home_team_id},
-      #{@away_team_id}
+      #{@away_team_id},
+      #{@home_team_score},
+      #{@away_team_score}
       ) RETURNING * ;"
     match = SqlRunner.run( sql ).first
     @id = match["id"]
