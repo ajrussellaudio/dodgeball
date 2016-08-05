4d3a34f4aa47e6d0db5fe40e7cd9a7c8c5df6e2e
Alan Russell <ajrussellaudio@gmail.com>
2016-08-05 12:03:02 +0100

    adds planning comments

---
+++ models/match.rb
@@ -0,0 +1,14 @@
+# Should have:
+# - id
+# - home_team_id
+# - away_team_id
+# - home_team_score
+# - away_team_score
+
+# Should be able to:
+# - Create new match
+# - Read match result
+# - Update match (with score?)
+# - Delete match (not really necessary, match declared void?)
+# - Return all matches
+# - Return both teams who played in a match (hash of :home and :away ?)
---
+++ models/team.rb
@@ -0,0 +1,10 @@
+# Should have:
+# - id
+# - name
+# ^ minimum
+
+# For league to work:
+# - points
+# - Wins, Losses, Draws
+# - Goals for, against
+
